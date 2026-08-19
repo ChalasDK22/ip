4b5a885906043975275c323c6942c36a302a89a9
Vu Dang Khoa <khoavudang0907@gmail.com>
2026-08-19 23:07:08 +0800

    change name into Eden

---
+++ src/main/java/Eden.java
@@ -0,0 +1,10 @@
+public class Eden {
+    public static void main(String[] args) {
+        String banner = " _____    _            \n"
+                + "| ____|__| | ___ _ __  \n"
+                + "|  _| / _` |/ _ \\ '_ \\ \n"
+                + "| |__| (_| |  __/ | | |\n"
+                + "|_____|\\__,_|\\___|_| |_|\n";
+        System.out.println(banner);
+    }
+}
