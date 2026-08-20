61db7b67852deaf4d67f90787251d7ce4c7dca56
Vu Dang Khoa <khoavudang0907@gmail.com>
2026-08-20 20:14:00 +0800

    Add greeting and farewell for Level-0

--- src/main/java/Eden.java
+++ src/main/java/Eden.java
@@ -1,3 +1,6 @@
+/**
+ * Greets the user as Eden and then displays a farewell message.
+ */
 public class Eden {
     public static void main(String[] args) {
         String banner = " _____    _            \n"
@@ -5,6 +8,15 @@ public class Eden {
                 + "|  _| / _` |/ _ \\ '_ \\ \n"
                 + "| |__| (_| |  __/ | | |\n"
                 + "|_____|\\__,_|\\___|_| |_|\n";
-        System.out.println(banner);
+        String line = "____________________________________________________________\n";
+        String greetings = "Hello! I'm Eden.\n"
+                + "What can I do for you?\n";
+        String bye = "Bye. Hope to see you again soon!\n";
+        System.out.print(line);
+        System.out.print(banner);
+        System.out.print(greetings);
+        System.out.print(line);
+        System.out.print(bye);
+        System.out.print(line);
     }
 }
