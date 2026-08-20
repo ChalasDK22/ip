f8873c2a7e230727d8a7c001d980507a4d7de795
Vu Dang Khoa <khoavudang0907@gmail.com>
2026-08-20 20:33:42 +0800

    Add in Echo for Level-1

--- src/main/java/Eden.java
+++ src/main/java/Eden.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Greets the user as Eden and then displays a farewell message.
  */
@@ -9,6 +10,9 @@ public class Eden {
                 + "| |__| (_| |  __/ | | |\n"
                 + "|_____|\\__,_|\\___|_| |_|\n";
         String line = "____________________________________________________________\n";
+        
+        Scanner scanner = new Scanner(System.in);
+        
         String greetings = "Hello! I'm Eden.\n"
                 + "What can I do for you?\n";
         String bye = "Bye. Hope to see you again soon!\n";
@@ -16,7 +20,20 @@ public class Eden {
         System.out.print(banner);
         System.out.print(greetings);
         System.out.print(line);
-        System.out.print(bye);
-        System.out.print(line);
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.print(line);
+                System.out.print(bye);
+                System.out.print(line);
+                break;
+            }
+
+            System.out.print(line);
+            System.out.print(command + " huh?\n");
+            System.out.print(line);
+        }
     }
 }
